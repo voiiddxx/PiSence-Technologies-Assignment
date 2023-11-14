daf94dbb51707731ab81f6a2128d9975ed8b5dfe
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-11-14 10:26:06 +0530

    added open close nav feature

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome'
 import { faBars, faUser} from '@fortawesome/free-solid-svg-icons'
 import Sidebar from './Sidebar'
 
 const Navbar = () => {
+
+    const [sideBarisOpneorNot, setsideBarisOpneorNot] = useState(false);
   return (
     <>
     <div className="navbar">
         <div className="nav-inner">
-        <FontAwesomeIcon icon={faBars} size='lg' color='black' />
-        <FontAwesomeIcon icon={faUser} size='lg' color='black' />
+        <FontAwesomeIcon className='button' onClick={()=>{
+          if(sideBarisOpneorNot===true){
+            setsideBarisOpneorNot(false);
+          }else{
+            setsideBarisOpneorNot(true);
+          }
+        }} icon={faBars} size='lg' color='black' />
+        <FontAwesomeIcon className='button' icon={faUser} size='lg' color='black' />
         </div>
     </div>
-    <Sidebar/>
+    {
+        sideBarisOpneorNot !== false ?  <Sidebar/> : <div></div>
+    }
     </>
+    
     
   )
 }
