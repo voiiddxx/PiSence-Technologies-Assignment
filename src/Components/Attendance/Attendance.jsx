d5e5ab805cf474f4f4416084e717123d5dceb75f
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-11-14 11:27:44 +0530

    added attendance conncetion

--- src/Components/Attendance/Attendance.jsx
+++ src/Components/Attendance/Attendance.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import "./Attendance.css"
 import data from "../../data (1).json"
 const Attendance = () => {
-    console.log(data['Behavioral Analytics']);
+    console.log(data);
+    
   return (
     <div className="attendance-main">
         <p>A T T E N D A N C E</p>
@@ -13,10 +14,64 @@ const Attendance = () => {
             </div>
             <div className="right">
             <div className="right-box">
-                    <h3>Sunday</h3>
-                    <p>21/03/2025 </p>
-                    <div className="green"></div>
-                    <div className="red"></div>
+                    <h3>Monday </h3>
+                    <p>{data[' ATTENDANCE'][' monday'].date} </p>
+                    {
+                        data[' ATTENDANCE'][' monday'].an==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                    {
+                        data[' ATTENDANCE'][' monday'].fn==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                 
+                </div>
+            <div className="right-box">
+                    <h3>Tuesday</h3>
+                    <p>{data[' ATTENDANCE'].tuesday.date}</p>
+                    {
+                        data[' ATTENDANCE'].tuesday.fn==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                    {
+                        data[' ATTENDANCE'].tuesday.an==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                </div>
+            <div className="right-box">
+                    <h3>Wednesday</h3>
+                    <p>{data[' ATTENDANCE'].wednesday.date}</p>
+
+                    {
+                        data[' ATTENDANCE'].wednesday.fn==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                   
+                    {
+                        data[' ATTENDANCE'].wednesday.an==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                   
+                </div>
+            <div className="right-box">
+                    <h3>Thrusday</h3>
+                    <p>{data[' ATTENDANCE'].thursday.date}</p>
+
+                    {
+                        data[' ATTENDANCE'].thursday.fn==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                   
+                    {
+                        data[' ATTENDANCE'].thursday.an==="present" ? <div className="green"></div> : <div className="red"></div>
+                    }
+                   
+                </div>
+            <div className="right-box">
+                    <h3>friday</h3>
+                    <p>{data[' ATTENDANCE'].friday.date}</p>
+
+{
+    data[' ATTENDANCE'].friday.fn==="present" ? <div className="green"></div> : <div className="red"></div>
+}
+
+{
+    data[' ATTENDANCE'].friday.an==="present" ? <div className="green"></div> : <div className="red"></div>
+}
+
                 </div>
             </div>
         </div>
